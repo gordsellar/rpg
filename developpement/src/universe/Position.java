931a844b6a648b3ec4abe884e2ed0d61923c6190
Pierre <pierre.sassoulas@gmail.com>
2013-12-28 23:09:49 +0100

    Début d'implémentation de entity et world (lot of todo)

---
+++ developpement/src/universe/Position.java
@@ -0,0 +1,22 @@
+package universe;
+
+/**
+ * @author pierre
+ * 
+ */
+
+public class Position {
+
+	public int x;
+	public int y;
+
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Position [x=" + x + ", y=" + y + "]";
+	}
+}
